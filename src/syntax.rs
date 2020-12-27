1f2c1a27c2c5b9aa25ddcc0887edcb92b2947157
Aaron <32745766+DaemonicWrath@users.noreply.github.com>
2020-12-27 18:40:58 -0500

    Add rowan syntaxkind

---
+++ src/syntax.rs
@@ -0,0 +1,52 @@
+use crate::lexer::TokenKind;
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[repr(u16)]
+pub enum SyntaxKind {
+    Whitespace,
+    Number,
+    Add,
+    Multiply,
+    LParen,
+    RParen,
+    Error,
+
+    List,
+    Literal,
+    Root,
+}
+
+impl From<TokenKind> for SyntaxKind {
+    fn from(token: TokenKind) -> Self {
+        match token {
+            TokenKind::Whitespace => SyntaxKind::Whitespace,
+            TokenKind::Number => SyntaxKind::Number,
+            TokenKind::Add => SyntaxKind::Add,
+            TokenKind::Multiply => SyntaxKind::Multiply,
+            TokenKind::LParen => SyntaxKind::LParen,
+            TokenKind::RParen => SyntaxKind::RParen,
+            TokenKind::Error => SyntaxKind::Error,
+        }
+    }
+}
+
+impl From<SyntaxKind> for rowan::SyntaxKind {
+    fn from(kind: SyntaxKind) -> Self {
+        Self(kind as u16)
+    }
+}
+
+pub type SyntaxNode = rowan::SyntaxNode<Lang>;
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub enum Lang {}
+
+impl rowan::Language for Lang {
+    type Kind = SyntaxKind;
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        assert!(raw.0 <= SyntaxKind::Root as u16);
+        unsafe { std::mem::transmute::<u16, SyntaxKind>(raw.0) }
+    }
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        rowan::SyntaxKind(kind as u16)
+    }
+}
